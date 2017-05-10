9d3c4e3e8672537b6974e46e2456db7d3fa69bd4
griffinj@lafayette.edu <griffinj@lafayette.edu>
2017-05-10 18:02:33 -0500

    Cleaning legacy prototyping code for the creation of Work and FileSet resources using HTTP requests offering the JSON media type

--- app/controllers/curation_concerns/file_sets_controller.rb
+++ app/controllers/curation_concerns/file_sets_controller.rb
@@ -1,6 +1,5 @@
 module CurationConcerns
   class FileSetsController < ApplicationController
     include CurationConcerns::FileSetsControllerBehavior
-    include LafayetteConcerns::FileSetsControllerBehavior
   end
 end
